93ba9d5bcdfa0c497feb3fc2a8700c35b8923606
Clóvis Reschke <clovisreschke@gmail.com>
2023-05-01 17:36:38 -0300

    Mqtt

--- david-feeder/src/main.cpp
+++ david-feeder/src/main.cpp
@@ -1,5 +1,21 @@
 #include <Arduino.h>
 #include <Servo.h>
+#include <MQTT.h>
+#include <ESP8266WiFi.h>
+
+void myDataCb(String &topic, String &data);
+void myPublishedCb();
+void myDisconnectedCb();
+void myConnectedCb();
+
+#define CLIENT_ID "rZYRpEtoMx"
+
+// create MQTT object
+MQTT myMqtt(CLIENT_ID, "broker.hivemq.com", 1883);
+
+//
+const char *ssid = "cmd.exe";
+const char *password = "rsk90000";
 
 #define TO_FEED D7
 #define LED D0
@@ -11,20 +27,36 @@ unsigned long current_time = millis();
 unsigned long last_trigger = 0;
 boolean timer_on = false;
 Servo servo;
+String mqttData;
 
-void feed()
+void feed(int qtd)
 {
-  int qtd = 1;
+
+  String topic("/");
+  topic += CLIENT_ID;
+  topic += "/value";
+
+  mqttData = "0";
+
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    myMqtt.publish(topic, mqttData);
+  }
   for (int i = 0; i < qtd; i++)
   {
-    for (int z = 0; z < 10; z++)
+    for (int z = 0; z < 2; z++)
     {
-
       servo.write(0);
-      delay(1000);
+      delay(500);
       servo.write(90);
-      delay(1000);
+      delay(500);
     }
+  }
+
+  mqttData = "1";
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    myMqtt.publish(topic, mqttData);
   }
 }
 
@@ -41,10 +73,9 @@ void led()
 
 IRAM_ATTR void inter()
 {
-  Serial.println("Motion was detected");
   digitalWrite(LED_0, HIGH);
-  timer_on = true;
-  last_trigger = millis();
+  feed(2);
+  digitalWrite(LED_0, LOW);
 }
 
 void setup()
@@ -58,15 +89,69 @@ void setup()
   servo.write(0);
   digitalWrite(LED, LOW);
   attachInterrupt(digitalPinToInterrupt(TO_FEED), inter, RISING);
+
+  delay(1000);
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.println("Connecting to MQTT server");
+
+  // setup callbacks
+  myMqtt.onConnected(myConnectedCb);
+  myMqtt.onDisconnected(myDisconnectedCb);
+  myMqtt.onPublished(myPublishedCb);
+  myMqtt.onData(myDataCb);
+
+  Serial.println("connect mqtt...");
+  myMqtt.connect();
+
+  delay(10);
 }
 
 void loop()
 {
-  current_time = millis();
-  if (timer_on && (current_time - last_trigger > interval))
-  {
-    Serial.println("Motion has stopped");
-    digitalWrite(LED_0, LOW);
-    timer_on = false;
-  }
+}
+
+/*
+ *
+ */
+void myConnectedCb()
+{
+  Serial.println("connected to MQTT server");
+}
+
+void myDisconnectedCb()
+{
+  Serial.println("disconnected. try to reconnect...");
+  delay(500);
+  myMqtt.connect();
+}
+
+void myPublishedCb()
+{
+  Serial.println("published.");
+}
+
+void myDataCb(String &topic, String &data)
+{
+
+  Serial.print(topic);
+  Serial.print(": ");
+  Serial.println(data);
 }
